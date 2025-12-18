a4860d7483668a91559ceb609ffcb1c9c796e7a8
Mahmoud Emam <laststand2231@gmail.com>
2025-12-18 15:33:04 +0200

    test: wire CSV read, fillna, describe, and export workflow

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+from dataframe import *
 def main():
+    df1 = DataFrame.read_csv(data_path = 'data/titanic.csv', dtype_path = 'data/titanic_dtype.csv')
     # TODO: Read data
-
 
     # TODO: Fill missing values
     # Numeric columns → mean
     # Categorical columns → mode
+    df1.fillna(num_strategy=get_col_mean,cat_strategy=get_col_mode)
+
 
 
     # TODO:Generate statistics file
+    df1.describe('describertest.csv')
 
 
     # TODO:Write cleaned data to CSV
+    df1.to_csv('testtyfinal.csv')
+
 
 
 
